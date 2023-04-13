0079a1e51b59903b1008457dd4403163da7e1599
Alan Chalker <github@chalker.org>
2023-04-12 20:40:04 -0400

    Create getimg.sh

---
+++ getimg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash       
+
+#The expected command line to execute this script is "./getimg.sh  cam\_name interval" with the following parameters:
+# - cam\_name corresponds to the hostname of the camera (e.g. XXXX\_cam1.osc.edu)
+# - interval corresponds to seconds delay. This will repeat for a total of 60 seconds. Typical usage is 10, meaning that one execution of the script will retrieve 6 images total (e.g. every 10 seconds over a minute).
+
+cam_name="$1"
+interval="$2"
+count=0
+while [[ $count -lt 60 ]]
+do
+        /usr/bin/curl -s http://$cam_name/axis-cgi/jpg/image.cgi --output \
+        PROJECTPATH/$cam_name/image_`date +%j-%H-%M-%S`.jpg
+        /usr/bin/sleep $interval
+        ((count=$count+$interval))
+done
